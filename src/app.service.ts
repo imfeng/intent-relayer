0c2053e5a37f9b634f1aa3d0973baa967eacade8
abner <fantasrulai@gmail.com>
2023-11-18 23:58:38 +0800

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -15,19 +15,44 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { TxTask } from './orm/tx-task.entity';
 import { ERC20PermitContractInterface } from './contracts/erc20-permit.abi';
 
+export enum CHAIN_ID {
+  GOERLI = 5,
+  ARBITRUM_GOERLI = 42161,
+  SCROLL_SEPOLIA = 534351,
+  LINEA_TESTNET = 59140,
+  POLYGON_ZKEVM_TESTNET = 1442,
+  GNOSIS = 100,
+  MANTLE_TESTNET = 5001,
+  BASE_GOERLI = 84531,
+}
+
 const CHAIN_MAP = {
-  5: 'https://ethereum-goerli.publicnode.com',
-  42161: 'https://arbitrum-one.publicnode.com	', // Arbitrum One Testnet
-  534351: 'https://scroll-sepolia.blockpi.network/v1/rpc/public', // Scroll sepolia
-  59140: 'https://rpc.goerli.linea.build', // Linea
-  1442: 'https://rpc.public.zkevm-test.net', // polygon zkEVM
-  100: 'https://1rpc.io/gnosis', // gnosis
+  [CHAIN_ID.GOERLI]: 'https://ethereum-goerli.publicnode.com',
+  [CHAIN_ID.ARBITRUM_GOERLI]: 'https://arbitrum-one.publicnode.com	', // Arbitrum One Testnet
+  [CHAIN_ID.SCROLL_SEPOLIA]:
+    'https://scroll-sepolia.blockpi.network/v1/rpc/public', // Scroll sepolia
+  [CHAIN_ID.LINEA_TESTNET]: 'https://rpc.goerli.linea.build', // Linea
+  [CHAIN_ID.POLYGON_ZKEVM_TESTNET]: 'https://rpc.public.zkevm-test.net', // polygon zkEVM
+  [CHAIN_ID.GNOSIS]: 'https://1rpc.io/gnosis', // gnosis
+  [CHAIN_ID.MANTLE_TESTNET]: 'https://rpc.testnet.mantle.xyz', // mantle testnet
+  [CHAIN_ID.BASE_GOERLI]: 'https://base-goerli.publicnode.com', // base goerli
+};
+
+export const ERC20PERMIT_ADDRESS = {
+  [CHAIN_ID.GOERLI]: '0x9Fda380dEE070cDc213840D770D5BD010B56D642',
+  [CHAIN_ID.ARBITRUM_GOERLI]: '0xeEd9Bc2C923f4d12fD857D73f5DC4736AF661784',
+  [CHAIN_ID.SCROLL_SEPOLIA]: '0x3f39d5652ad8665b663869f3231cd131a0ec909d',
+  [CHAIN_ID.LINEA_TESTNET]: '0x419c2e4d9e9d272e20c894c788f31cf7ed1c1eaf',
+  [CHAIN_ID.POLYGON_ZKEVM_TESTNET]:
+    '0x419c2e4d9e9d272e20c894c788f31cf7ed1c1eaf',
+  [CHAIN_ID.GNOSIS]: '0x419c2e4d9e9d272e20c894c788f31cf7ed1c1eaf',
+  [CHAIN_ID.MANTLE_TESTNET]: '0x3e2229ab2dcc01f6df19fa189e3025c14a816816',
+  [CHAIN_ID.BASE_GOERLI]: '0x3E2229AB2DCC01F6DF19fa189E3025c14A816816',
 };
 
 @Injectable()
 export class AppService {
   signer: Wallet;
-  ERC20PERMIT_ADDRESS!: string;
   constructor(
     private readonly configService: ConfigService,
     private readonly logger: CustomLogger,
@@ -35,9 +60,6 @@ export class AppService {
     private readonly txTaskRepository: Repository<TxTask>,
   ) {
     const priv = this.configService.getOrThrow<string>('PRIV');
-    this.ERC20PERMIT_ADDRESS = this.configService.getOrThrow<string>(
-      'ERC20PERMIT_ADDRESS',
-    );
     // const contactA
     this.signer = new Wallet(priv);
     this.test();
@@ -70,15 +92,17 @@ export class AppService {
   }
 
   async doPermit(txDto: RelayerTxDto) {
+    const provideUrl = this.getProviderUrl(txDto.chainId);
     if (txDto.value !== '0') {
       throw new BadRequestException('value must be 0');
     }
-    if (txDto.toAddress !== this.ERC20PERMIT_ADDRESS) {
-      throw new BadRequestException(
-        `toAddress must be ${this.ERC20PERMIT_ADDRESS}`,
-      );
+    const permitAddress = ERC20PERMIT_ADDRESS[txDto.chainId];
+    if (!permitAddress) {
+      throw new BadRequestException(`unsupported chain=${txDto.chainId}`);
     }
-    const provideUrl = this.getProviderUrl(txDto.chainId);
+    if (txDto.toAddress !== permitAddress) {
+      throw new BadRequestException(`toAddress must be ${permitAddress}`);
+    }
 
     const provider = new JsonRpcProvider(provideUrl);
     const {
@@ -107,7 +131,7 @@ export class AppService {
       .connect(provider)
       .sendTransaction({
         from: this.signer.address,
-        to: this.ERC20PERMIT_ADDRESS,
+        to: permitAddress,
         // gasPrice: txDto.fee,
         // value: txDto.value,
         data: txDto.calldata,
@@ -125,7 +149,7 @@ export class AppService {
       .connect(provider)
       .sendTransaction({
         from: this.signer.address,
-        to: this.ERC20PERMIT_ADDRESS,
+        to: permitAddress,
         data: transferCalldata,
       });
     this.logger.log(
